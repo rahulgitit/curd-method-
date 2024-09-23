2933ad00e83e6c35e3db426c259180a3cd5c76da
rahuugitit <rk8449022182@gmail.com>
2024-09-23 11:38:43 +0530

    add the new features

--- todo/urls.py
+++ todo/urls.py
@@ -8,7 +8,7 @@ from enroll import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("home/", views.index, name="index"),
-    path("delete/<int:id>/", views.delete,name="delete"),
+    path("delete/<int:id>/", views.delete,name="delete"), # type: ignore
     path("edit/<int:id>/", views.edit,name="edit"),
     path("api/", include('enroll.api.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
